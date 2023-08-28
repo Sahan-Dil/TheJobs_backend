bbca512dc7d5687b04ba4b08defe5d9651d5c7ba
Sahan <sahandilshan.projects.gmail.com>
2023-08-28 13:48:29 +0530

    crud for consultant

--- src/main/java/com/SahanDilshan/TheJobs/controllers/ConsultantController.java
+++ src/main/java/com/SahanDilshan/TheJobs/controllers/ConsultantController.java
@@ -1,18 +1,58 @@
 package com.SahanDilshan.TheJobs.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SahanDilshan.TheJobs.models.ApplicationUser;
+import com.SahanDilshan.TheJobs.models.Consultancy;
+import com.SahanDilshan.TheJobs.models.ConsultancyDTO;
+import com.SahanDilshan.TheJobs.services.ConsultancyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/consultant")
 @CrossOrigin("*")
 public class ConsultantController {
 
+    @Autowired
+    private ConsultancyService consultancyService;
+
     @GetMapping("/")
     public String helloAdminController(){
         return "Consultant";
     }
+
+    @PostMapping("/createConsultancy")
+    public Consultancy createConsultancy(
+            @RequestBody ConsultancyDTO consultancyDTO
+
+    ) {
+        return consultancyService.createConsultancy(consultancyDTO);
+    }
+
+    @PutMapping("/updateConsultancy/{consultancyId}")
+    public Consultancy updateConsultancy(
+            @PathVariable Integer consultancyId,
+            @RequestBody ConsultancyDTO consultancyDTO
+    ) {
+        return consultancyService.updateConsultancy(consultancyId, consultancyDTO);
+    }
+
+    @GetMapping("/getConsultancy/{userId}")
+    public ResponseEntity<?> getConsultancyByUserId(@PathVariable Integer userId) {
+        Consultancy consultancy = consultancyService.getConsultancyByUserId(userId);
+
+        if (consultancy != null) {
+            return ResponseEntity.ok(consultancy);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/deleteConsultancy/{userId}")
+    public String deleteConsultancyByUserId(@PathVariable Integer userId) {
+        return consultancyService.deleteConsultancyByUserId(userId);
+    }
+
 
 }
---
+++ src/main/java/com/SahanDilshan/TheJobs/services/ConsultancyService.java
@@ -0,0 +1,66 @@
+package com.SahanDilshan.TheJobs.services;
+
+import com.SahanDilshan.TheJobs.models.ApplicationUser;
+import com.SahanDilshan.TheJobs.models.Consultancy;
+import com.SahanDilshan.TheJobs.models.ConsultancyDTO;
+import com.SahanDilshan.TheJobs.repository.ConsultancyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ConsultancyService {
+
+    @Autowired
+    private ConsultancyRepository consultancyRepository;
+
+    public Consultancy createConsultancy(ConsultancyDTO consultancyDTO) {
+        Integer userId = consultancyDTO.getUserId();
+        if (consultancyRepository.existsById(userId)) {
+            throw new IllegalStateException("A record already exists for you!!!.");
+        }
+
+        Consultancy consultancy = new Consultancy();
+        consultancy.setName(consultancyDTO.getName());
+        consultancy.setCountry(consultancyDTO.getCountry());
+        consultancy.setGender(consultancyDTO.getGender());
+        consultancy.setDescription(consultancyDTO.getDescription());
+        consultancy.setPhone(consultancyDTO.getPhone());
+        consultancy.setEmail(consultancyDTO.getEmail());
+        consultancy.setUserId(consultancyDTO.getUserId());
+
+        return consultancyRepository.save(consultancy);
+    }
+
+    public Consultancy updateConsultancy(Integer consultancyId, ConsultancyDTO consultancyDTO) {
+        Consultancy existingConsultancy = consultancyRepository.findById(consultancyId)
+                .orElseThrow(() -> new IllegalStateException("Can't found record!!!"));
+
+        existingConsultancy.setName(consultancyDTO.getName());
+        existingConsultancy.setCountry(consultancyDTO.getCountry());
+        existingConsultancy.setGender(consultancyDTO.getGender());
+        existingConsultancy.setDescription(consultancyDTO.getDescription());
+        existingConsultancy.setPhone(consultancyDTO.getPhone());
+        existingConsultancy.setEmail(consultancyDTO.getEmail());
+        existingConsultancy.setUserId(consultancyDTO.getUserId());
+
+        return consultancyRepository.save(existingConsultancy);
+    }
+
+    public Consultancy getConsultancyByUserId(Integer userId) {
+        return consultancyRepository.findByUserId(userId);
+    }
+
+    public String  deleteConsultancyByUserId(Integer userId) {
+        Optional<Consultancy> existingConsultancy = Optional.ofNullable(consultancyRepository.findByUserId(userId));
+        if (existingConsultancy.isPresent()) {
+            consultancyRepository.delete(existingConsultancy.get());
+            return "Deleted";
+        } else {
+            return "Not found";
+        }
+    }
+
+
+}
