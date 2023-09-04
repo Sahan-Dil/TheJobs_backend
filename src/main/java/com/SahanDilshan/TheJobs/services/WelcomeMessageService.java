60d32e04c3b7da08c74e46051baf53a398075e0b
Sahan <sahandilshan.projects.gmail.com>
2023-09-04 23:23:32 +0530

    create welcome message methods....

--- src/main/java/com/SahanDilshan/TheJobs/services/WelcomeMessageService.java
+++ src/main/java/com/SahanDilshan/TheJobs/services/WelcomeMessageService.java
@@ -1,6 +1,5 @@
 package com.SahanDilshan.TheJobs.services;
 
-// WelcomeMessageService.java
 import com.SahanDilshan.TheJobs.models.WelcomeMessageEntity;
 import com.SahanDilshan.TheJobs.repository.WelcomeMessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,21 @@ public class WelcomeMessageService {
 
     @Autowired
     private WelcomeMessageRepository welcomeMessageRepository;
+
+    // Step 2: Create a private static instance variable
+    private static WelcomeMessageService instance;
+
+    // Step 1: Private constructor to prevent external instantiation
+    private WelcomeMessageService() {
+    }
+
+    // Step 3: Provide a public static method to access the single instance
+    public static synchronized WelcomeMessageService getInstance() {
+        if (instance == null) {
+            instance = new WelcomeMessageService();
+        }
+        return instance;
+    }
 
     public String getWelcomeMessage() {
         Optional<WelcomeMessageEntity> optionalMessage = welcomeMessageRepository.findById(1L); // Assuming message ID is 1
